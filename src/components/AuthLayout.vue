02610c6ba91f5f783191eb6d56934a2300fdc078
alexander <zen99040@icloud.com>
2026-04-04 10:26:34 -0500

    add bootstrap style to login page

--- src/components/AuthLayout.vue
+++ src/components/AuthLayout.vue
@@ -5,7 +5,9 @@ defineProps<{
 </script>
 
 <template>
-  <div class="min-vh-100 d-flex align-items-center justify-content-center py-5">
+  <div
+    class="min-vh-100 d-flex align-items-center justify-content-center py-5 bg-body-secondary"
+  >
     <div class="w-100 px-3 auth-inner">
       <h1 class="h3 mb-3 fw-normal text-center">{{ title }}</h1>
       <slot />
